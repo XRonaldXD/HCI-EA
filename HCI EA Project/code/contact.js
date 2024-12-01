aada3db052f84286e05a238accbe33a902647a06
XRonaldXD <shamhoikin0308@gmail.com>
2024-12-01 23:58:27 +0800

    modify all herf to car

--- HCI EA Project/code/contact.js
+++ HCI EA Project/code/contact.js
@@ -1,6 +1,5 @@
 document.getElementById("contactForm").addEventListener("submit", function(e) {
     e.preventDefault();
-    // Here you could handle form submission, e.g., send data to a server
     alert("Thank you for your message! We will get back to you shortly.");
     this.reset(); // Reset the form after submission
 });
